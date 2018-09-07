32c428b48ee0941bab6f317f4e8bfa06f001a791
paxarpp <34523588+paxarpp@users.noreply.github.com>
2018-09-08 00:03:49 +0300

    added request

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from "axios";
 
-export default class App extends React.Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    const baseURL = "https://reddit.com/r/aww.json";
+    axios.get(baseURL)
+      .then(response => this.setState({posts: response.data.data.children}))
+      .catch(err => console.log(err))
+  }
+
   render() {
-    return (<div>
-      <h2>Welcome to React!</h2>
-    </div>);
+      const { posts } = this.state;
+    return <div>
+        <h2>Welcome to React!</h2>
+        {posts.length !== 0 ? posts.map(post => (
+              <div
+                style={{height: '60px'}}
+              >
+                <img
+                  src={post.data.preview.images[0].source.url}
+                  style={{
+                    width: '30px',
+                    height: '30px',
+                    borderRadius: '50%'
+                  }}
+                />
+                {post.data.title}
+              </div>
+            )) : '...loading'}
+      </div>;
   }
 }
