9190f4f853c83bd05668e7583afa47ee7908f11a
paxarpp <pavelpp1974@gmail.com>
2018-09-08 08:48:52 +0300

    beg

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    axios.get("https://reddit.com/r/aww.json")
+      .then(res => this.setState({posts: res.data.data.children}))
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (<div>
       <h2>Welcome to React!</h2>
