4ed456fc81ebaf268412d478ab85f2ff3ecaad5f
paxarpp <pavelpp1974@gmail.com>
2018-09-14 22:23:26 +0300

    cheged metod without this to static

--- src/Main.jsx
+++ src/Main.jsx
@@ -13,50 +13,7 @@ const { Menu } = electron.remote;
 
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: [],
-      subreddit: [],
-      headerNews: {
-        title: null,
-        id: null,
-      },
-    };
-    this.baseURL = 'https://old.reddit.com/';
-
-    this.choiseHead = this.choiseHead.bind(this);
-    this.showImage = this.showImage.bind(this);
-    this.initMenu = this.initMenu.bind(this);
-  }
-
-  componentDidMount() {
-    this.initMenu();
-
-    const listsTitleNews = 'subreddits.json';
-    axios.get(`${this.baseURL}${listsTitleNews}`)
-      .then(response => this.setState({ subreddit: response.data.data.children }))
-      .catch(err => console.log(err));
-  }
-
-  choiseHead(data) {
-    const head = data.display_name;
-    const { id } = data;
-    const headerNews = { id, title: head };
-    const request = `r/${head}.json`;
-    axios.get(`${this.baseURL}${request}`)
-      .then(response => this.setState({
-        posts: response.data.data.children,
-        headerNews,
-      }))
-      .catch(err => console.log(err));
-  }
-
-  showImage(image) {
-    ipcRenderer.send('toggle-image', image);
-  }
-
-  initMenu() {
+  static initMenu() {
     const menu = Menu.buildFromTemplate([
       {
         label: 'File',
@@ -88,6 +45,47 @@ class Main extends Component {
     Menu.setApplicationMenu(menu);
   }
 
+  static showImage(image) {
+    ipcRenderer.send('toggle-image', image);
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      subreddit: [],
+      headerNews: {
+        title: null,
+        id: null,
+      },
+    };
+    this.baseURL = 'https://old.reddit.com/';
+
+    this.choiseHead = this.choiseHead.bind(this);
+  }
+
+  componentDidMount() {
+    Main.initMenu();
+
+    const listsTitleNews = 'subreddits.json';
+    axios.get(`${this.baseURL}${listsTitleNews}`)
+      .then(response => this.setState({ subreddit: response.data.data.children }))
+      .catch(err => console.log(err));
+  }
+
+  choiseHead(data) {
+    const head = data.display_name;
+    const { id } = data;
+    const headerNews = { id, title: head };
+    const request = `r/${head}.json`;
+    axios.get(`${this.baseURL}${request}`)
+      .then(response => this.setState({
+        posts: response.data.data.children,
+        headerNews,
+      }))
+      .catch(err => console.log(err));
+  }
+
   render() {
     const { posts, subreddit, headerNews } = this.state;
     return (
@@ -98,7 +96,7 @@ class Main extends Component {
 
         {
           posts.length !== 0 ? posts.map(post => (
-            <Post key={post.data.id} post={post} onClick={this.showImage} />
+            <Post key={post.data.id} post={post} onClick={Main.showImage} />
           )) : <div>...loading</div>
         }
       </div>
